2c5155f659502853d0ded09e0d57a7722157dc25
Ammar Nagri <ammar.nagri1@gmail.com>
2025-06-04 02:03:07 +0100

    Set-up
    I created setup_days.py which uses requests to load a set template for all the solutions to the and the input texts within the respective folders.

---
+++ Day_16/solution.py
@@ -0,0 +1,8 @@
+with open("input.txt") as f:
+    lines = f.readlines()
+
+def part_1():
+    ...
+
+def part_2():
+    ...
---
+++ setup_days.py
@@ -0,0 +1,47 @@
+import requests 
+import os 
+
+TEMPLATE = """with open("input.txt") as f:
+    lines = f.readlines()
+
+def part_1():
+    ...
+
+def part_2():
+    ...
+"""
+
+
+def download_input(day: int) -> bytes:
+    URL = f"https://adventofcode.com/2024/day/{day}/input"
+    #Reads in the auth key required to get input data
+    with open(".session","r") as f:
+        session =f.read().strip()
+    #Reads in the auth key required to get input data
+    cookies = {"session" :  session}
+    
+    resp = requests.get(url=URL,
+                       cookies=cookies)
+    
+    return resp.content 
+
+def setup_day(day: int) -> None :
+    origin_dir = os.getcwd()  
+    dir_name = f"Day_{day}"
+
+    os.mkdir(dir_name)
+
+    data = download_input(day=day).decode()
+
+    os.chdir(dir_name)
+
+    with open("solution.py","w") as f:
+        f.write(TEMPLATE)
+    with open("data.txt","w") as f:
+        f.write(data)
+
+    os.chdir(origin_dir)
+
+    
+for day in range(1,26):
+    setup_day(day=day)
